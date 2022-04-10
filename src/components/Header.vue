54e4765d149a69baa89bdd476309151df4a7aef9
Sayuru De Alwis <saurudealwis@ymail.com>
2022-04-10 12:41:32 +0530

    Upload

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button @toggle-add-task="$emit('btn-click')" :text="showAddTask ? 'close' : 'Add Task'" color="green" />
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default  {
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean
+    },
+components: {
+    Button,
+},
+}
+</script>
+
+<style scoped>
+    header  {
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+        margin-bottom: 20px;
+    }
+</style>
